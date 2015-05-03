3c390c7f1eca4da0a82f10f250ee649b7fa9a484
knighoffnana <revolution318@gmail.com>
2015-05-03 13:11:58 +0800

    rm index.php

--- application/views/index.php
+++
@@ -1,7 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<form name="form" method="post" action="connect.php">
-<input type="text" name="memID" /> <br>
-<input type="password" name="memKey" /> <br>
-<input type="submit" name="button" value="登入" />&nbsp;&nbsp;
-<a href="add.php">申請帳號</a>
-</form>
