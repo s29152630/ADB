1fe2436a493cda7916a0fa1a42813073cecde310
knighoffnana <revolution318@gmail.com>
2015-05-06 16:56:09 +0800

    刪除多餘的選項

--- application/views/inquire_room.php
+++ application/views/inquire_room.php
@@ -26,8 +26,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <select name="roomCapacity">
 	<option value="2" <?php echo set_select('roomCapacity', '2', TRUE); ?> >2</option>
 	<option value="4" <?php echo set_select('roomCapacity', '4'); ?> >4</option>
-	<option value="6" <?php echo set_select('roomCapacity', '6'); ?> >6</option>
-	<option value="8" <?php echo set_select('roomCapacity', '8'); ?> >8</option>
 </select>
 </br>
 
@@ -35,8 +33,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <select name="roomPrice">
 	<option value="2000" <?php echo set_select('roomPrice', '2000', TRUE); ?> >$2000</option>
 	<option value="4000" <?php echo set_select('roomPrice', '4000'); ?> >$4000</option>
-	<option value="6000" <?php echo set_select('roomPrice', '6000'); ?> >$6000</option>
-	<option value="8000" <?php echo set_select('roomPrice', '8000'); ?> >$8000</option>
 </select>
 </br>
 
@@ -46,9 +42,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<option value="橙海風格" <?php echo set_select('roomStyle', '橙海風格'); ?> >橙海風格</option>
 	<option value="黃海風格" <?php echo set_select('roomStyle', '黃海風格'); ?> >黃海風格</option>
 	<option value="綠海風格" <?php echo set_select('roomStyle', '綠海風格'); ?> >綠海風格</option>
-	<option value="藍海風格" <?php echo set_select('roomStyle', '藍海風格'); ?> >藍海風格</option>
-	<option value="靛海風格" <?php echo set_select('roomStyle', '靛海風格'); ?> >靛海風格</option>
-	<option value="紫海風格" <?php echo set_select('roomStyle', '紫海風格'); ?> >紫海風格</option>
 </select>
 </br>
 
