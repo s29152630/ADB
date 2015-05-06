5916f24967afbde64836a77506370c62d1e22a96
knighoffnana <revolution318@gmail.com>
2015-05-06 21:11:02 +0800

    完整版ver1.0

--- application/views/edit_record.php
+++ application/views/edit_record.php
@@ -9,27 +9,31 @@
 <?php
 include("SQL.php");
     
-    $sql_1='SELECT `roomID`,`payDay`,`checkinDate`,`checkoutDate`,`recID`,`recDate` FROM `record`'; 
+    $sql_1='SELECT * FROM `record` NATURAL JOIN `bookingdate`' ; 
 
-    echo '<div style="ZOOM: 200% ;font-weight: bold;background-color: hsla(210, 80%, 50%, 0.075); margin: 0px auto; width:455px; border-radius: 8px;margin-top:200px;">';
+    echo '<div style="ZOOM: 200% ;font-weight: bold;background-color: hsla(210, 80%, 50%, 0.075); margin: 0px auto; width:595px; border-radius: 8px;margin-top:200px;">';
     echo form_open('repair/editRecord2');
         echo '<select name="recID">';
         $result_1 = mysqli_query($con, $sql_1); 
             if(mysqli_num_rows($result_1)>0){  
                 while ($row = mysqli_fetch_object($result_1)){
+                    
+                    $memID=$row->memID;
+                    $startDate=$row->startDate;
                     $roomID=$row->roomID;
                     $recID=$row->recID;
                     $recDate=$row->recDate;
                     $payDay=$row->payDay;
                     $checkinDate=$row->checkinDate;
                     $checkoutDate=$row->checkoutDate;
-                    echo '<option value="'.$recID.'">房號'.$roomID.',付款時間'.$payDay.',入住時間'.$checkinDate.',退房時間'.$checkoutDate.'</option>';
+                    echo '<option value="'.$recID.'">會員'.$memID.',訂房時間'.$startDate.',房號'.$roomID.',付款時間'.$payDay.',入住時間'.$checkinDate.',退房時間'.$checkoutDate.'</option>';
                 }
             }
         echo '</select>';
 
+    echo '<div style="text-align:center;">';
     echo '<input style="font-family:Microsoft JhengHei; font-weight: bold;background-color: hsla(210, 80%, 50%, 0.075);  border-radius: 4px;" type="submit" value="修改">'; 
-       
+    echo '</div>';
 
 
 
